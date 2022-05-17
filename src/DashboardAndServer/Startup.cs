6d1c55b2f8d266efe79f6347d0775d8316c29e52
Oscar D. Fortaleché <lechediaz@gmail.com>
2022-05-16 23:15:43 -0500

    Primer commit
    - Contiene workflow de endpoint con Hello World de ejemplo.
    - Contiene prueba de workflow recurrente, pero parece tener bug y queda ejecutandose por segundo en vez de minutos.

---
+++ src/DashboardAndServer/Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Elsa;
+using Elsa.Persistence.EntityFramework.Core.Extensions;
+using Elsa.Persistence.EntityFramework.Sqlite;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DashboardAndServer
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        private IWebHostEnvironment Environment { get; }
+        private IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var elsaSection = Configuration.GetSection("Elsa");
+
+            // Elsa services.
+            services
+                .AddElsa(elsa => elsa
+                    .UseEntityFrameworkPersistence(ef => ef.UseSqlite())
+                    .AddConsoleActivities()
+                    .AddHttpActivities(elsaSection.GetSection("Server").Bind)
+                    .AddQuartzTemporalActivities()
+                    .AddWorkflowsFrom<Startup>()
+                );
+
+            // Elsa API endpoints.
+            services.AddElsaApiEndpoints();
+
+            // For Dashboard.
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+               .UseStaticFiles() // For Dashboard.
+               .UseHttpActivities()
+               .UseRouting()
+               .UseEndpoints(endpoints =>
+               {
+                    // Elsa API Endpoints are implemented as regular ASP.NET Core API controllers.
+                    endpoints.MapControllers();
+
+                    // For Dashboard.
+                    endpoints.MapFallbackToPage("/_Host");
+               });
+        }
+    }
+}
