08c8801dae1e5452c7dd3960fc324a91c6882f2a
Oscar D. Fortaleché <lechediaz@gmail.com>
2022-05-25 17:23:15 -0500

    Renombro la carpeta donde tengo el servidor y dashboard de Elsa

--- src/DashboardAndServer/Startup.cs
+++ src/DashboardAndServer/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace DashboardAndServer
+namespace ElsaDashboardAndServer
 {
     public class Startup
     {
