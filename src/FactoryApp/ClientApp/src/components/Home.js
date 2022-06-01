471880017f43c11c43c3914d6a5f40b699165bfb
Oscar D. Fortaleché <lechediaz@gmail.com>
2022-06-01 00:26:58 -0500

    Se configuran variables de ambiente para react desde archivos Dockerfile y docker-compose

--- src/FactoryApp/ClientApp/src/components/Home.js
+++ src/FactoryApp/ClientApp/src/components/Home.js
@@ -7,6 +7,7 @@ export class Home extends Component {
     return (
       <div>
         <h1>Hello, world!</h1>
+        <h2>ELSA_SERVER: {process.env.REACT_APP_ELSA_SERVER}</h2>
         <p>Welcome to your new single-page application, built with:</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
